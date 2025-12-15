e728d4f7223f1cbbc79789fc1ca0fb85972464db
WoutHofstra <hofstrawout@gmail.com>
2025-12-15 19:00:48 +0200

    Added migrations

--- Infrastructure/Migrations/20251215165903_AddIsCompletedToTasks.Designer.cs
+++ Infrastructure/Migrations/20251215165903_AddIsCompletedToTasks.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using myRESTAPI.Infrastructure.Persistence;
 
@@ -10,9 +11,11 @@ using myRESTAPI.Infrastructure.Persistence;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(TaskDbContext))]
-    partial class TaskDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251215165903_AddIsCompletedToTasks")]
+    partial class AddIsCompletedToTasks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.22");
