494e2602efc00cd3564697a0e1bdf2246ddae3c8
WoutHofstra <hofstrawout@gmail.com>
2025-12-15 18:10:54 +0200

    fixing azure db bug

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06418e83a9595e4fadf9366f31ce39a1f4d447dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f8d45b5c3def3d29d0a377756676a75d4f51eb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
