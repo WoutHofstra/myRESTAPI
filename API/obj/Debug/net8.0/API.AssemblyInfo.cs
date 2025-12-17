0820370cbd5137ea4f3dcb2b86633f318eb2b4ab
WoutHofstra <hofstrawout@gmail.com>
2025-12-17 16:34:39 +0200

    Add npsql to API layer

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b3a7fefa7861c68becd33a39c0dd5dda156bd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c964951cda79dc254fe1a9fdc6588ad96138e34")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
