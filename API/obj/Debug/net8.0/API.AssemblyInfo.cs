bae9454da5704751e9e6ce740bdb5ecabb0109d8
WoutHofstra <hofstrawout@gmail.com>
2025-12-19 16:01:13 +0200

    Pushing to github for azure deployment

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c611bc88c162efa9011a1ea98f5ae3df33872f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d6934a49d6ef43d0dc15941449abc555803be9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
